80d6822208980572fc3cb56e907c4763e0e24476
Dave Syer <dsyer@vmware.com>
2021-03-12 17:59:02 +0000

    Add CborTests (disabled)

---
+++ server/src/test/java/org/springframework/mock/rsocket/CborTests.java
@@ -0,0 +1,48 @@
+package org.springframework.mock.rsocket;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.rsocket.RSocketRequester;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@ExtendWith(RSocketServerExtension.class)
+@Disabled
+class CborTests {
+
+	private RSocketRequester rsocketRequester;
+
+	public CborTests(@Autowired RSocketRequester.Builder rsocketRequesterBuilder,
+			@Value("${rsocket.host:localhost}") String host,
+			@Value("${test.rsocket.server.port:7000}") int port) {
+		rsocketRequester = rsocketRequesterBuilder.tcp(host, port);
+	}
+
+	@Test
+	void response(RSocketMessageRegistry catalog) {
+		MessageMapping response = MessageMapping.response("response")
+				.response(new Foo("Server", "Response"));
+		catalog.register(response);
+		assertThat(rsocketRequester.route("response").data(new Foo("Client", "Request"))
+				.retrieveMono(Foo.class).doOnNext(foo -> {
+					System.err.println(foo);
+					assertThat(foo.getOrigin()).isEqualTo("Server");
+				}).block()).isNotNull();
+		assertThat(response.drain()).hasSize(1);
+		assertThat(response.drain()).hasSize(0);
+	}
+
+	@EnableAutoConfiguration
+	@Configuration
+	static class Application {
+	}
+
+}
